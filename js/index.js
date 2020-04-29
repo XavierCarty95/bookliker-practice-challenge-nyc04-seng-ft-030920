8b9afeeb9ff03fed48f272bb86433c447141c4ff
Xavier Carty <xaviercarty@gmail.com>
2020-04-29 09:14:06 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,81 @@
-document.addEventListener("DOMContentLoaded", function() {});
+let bookList = document.getElementById("list")
+
+
+fetch("http://localhost:3000/books")
+.then(r => r.json())
+.then(booksResponse => {
+    booksResponse.forEach(element => {
+        render(element)
+    })
+})
+
+
+
+function render(books)
+{
+    
+    let header = document.createElement('h2')
+    header.innerText = books.title
+
+    let bookDescriptions = document.createElement("p")
+    bookDescriptions.innerText = books.description
+    
+    let img = document.createElement('img')
+    img.src = books.img_url
+    img.alt = "pictures of books"
+
+    bookList.append(header , bookDescriptions , img)
+
+    books.users.forEach( usernames => {
+
+        let bookLi = document.createElement("li")
+        bookLi.innerText = "Username :  " + usernames.username
+        bookList.append(bookLi)
+
+    })
+
+    let likeButton = document.createElement('button')
+    likeButton.innerText = "Like"
+    bookList.append(likeButton)
+     
+
+    likeButton.addEventListener("click" , function(evt) {
+        
+        let newLikedUsers = [...books.users, {"id":1, username:"pouros" }] 
+        
+        fetch(`http://localhost:3000/books/${books.id}`, {
+
+              method: "PATCH",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+               id: books.id, 
+               "users": newLikedUsers 
+              }
+
+              )
+
+
+
+        }).then(r => r.json())
+        .then(booksObj => {
+             
+            books.users.push(booksObj.newLikedUsers)
+
+            books.users = booksObj.users
+            console.log(booksObj)
+
+        })
+
+
+
+
+
+    })
+
+
+
+
+
+}
